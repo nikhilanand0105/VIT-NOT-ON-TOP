ceca603d2f2df1ed1c992e50c312283edde71fb4
nikhilanand0105 <nikhilanand0105@gmail.com>
2024-07-31 00:45:02 +0530

    Add files via upload

---
+++ VTOP/login/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+
+def student(request):
+    return render(request,'login.html')
+
+
+def home(request):
+    return render(request,"index.html")
