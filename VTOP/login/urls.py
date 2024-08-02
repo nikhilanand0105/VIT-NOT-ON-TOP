16de36af8d10faea9f00fa4f737a1f1c2d7686e8
nikhilanand0105 <nikhilanand0105@gmail.com>
2024-08-02 12:11:37 +0530

    Update urls.py

--- VTOP/login/urls.py
+++ VTOP/login/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns=[
     path('',views.home,name="home"),
-    path("login/",views.student,name="student")
+    path("login/",views.student,name="student"),
+    path("error/",views.error,name="error")
 
 ]
